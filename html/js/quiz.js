559236932e0b1ed128686d3e050f10f011b2aeb7
Christian Schünemann <christian@schuenemann.me>
2025-04-09 09:14:48 +0200

    progressbar

--- html/js/quiz.js
+++ html/js/quiz.js
@@ -122,8 +122,8 @@ window.nextQuestion = function nextQuestion() {
     if (questionCount >= maxQuestions) {
         showFinalScore();
     } else {
-        updateProgressBar();
         loadNewQuestion();
+        updateProgressBar();
     }
 }
 
@@ -220,7 +220,7 @@ window.resetUI = function resetUI() {
 };
 
 window.updateProgressBar = function updateProgressBar() {
-    const percent = ((questionCount +1 )/ maxQuestions) * 100;
+    const percent = ((questionCount)/ maxQuestions) * 100;
     document.getElementById('progressBar').style.width = `${percent}%`;
     document.getElementById('progressBarContainer').style.display = 'block';
 }
